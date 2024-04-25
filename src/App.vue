e96727038658d3587b1e71db695d86a17f424a67
IronMan0602 <55095834+cessDevs1419@users.noreply.github.com>
2024-04-25 08:45:19 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import NavbarComponent from './components/components/NavbarComponent.vue';
+</script>
+
+<template>
+  <NavbarComponent>
+  </NavbarComponent>
+  <RouterView></RouterView>
+  <RouterView />
+</template>
+
+
---
+++ src/components/components/NavbarComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid d-flex justify-content-between">
+            <h1 class="navbar-brand fs-2 fw-bold primary-font" href="#">F<span class="secondary-font fw-bold">C</span></h1>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="w-100 d-flex align-items-lg-center align-items-start flex-lg-row flex-column-reverse">
+                    <div class="w-100 d-flex justify-content-start justify-content-lg-center">
+                        <div class="navbar-nav">
+                            <a class="nav-link fw-semibold" >Home</a>
+                            <a class="nav-link fw-semibold" >About</a>
+                            <a class="nav-link fw-semibold" >Project</a>
+                            <a class="nav-link fw-semibold">Contact</a>
+                        </div>
+                    </div>
+                    <button class="btn btn-mode border bg-white rounded-circle p-3 " type="button" >
+
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style>
+.btn-mode{
+    width: 100%;
+    max-width: 15px;
+    min-width: 15px;
+    height: 15px;
+}
+</style>
