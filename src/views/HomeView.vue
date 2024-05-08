a3bda66b6a3c48aeeccd256728c94c89aa3a0d17
IronMan0602 <55095834+cessDevs1419@users.noreply.github.com>
2024-05-08 16:31:19 +0800

    added shaking hand svg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,34 @@
 <template>
-    <div class="hero w-100 primary-color" v-bind:class="[dark_mode ? 'primary-color':'bg-white']">
+    <div class="hero d-flex align-items-center justify-content-between w-100 primary-color" v-bind:class="[dark_mode ? 'primary-color':'bg-white']">
+        <div class="socmed h-100">
 
+        </div>
+        <div class="title-container d-flex flex-column align-items-center">
+            <div class="intro-title d-flex m-auto">
+                <h5 class="fw-semibold intro-title  m-0 me-3" v-bind:class="[dark_mode ? 'text-white':'primary-font']">Hello There</h5>
+                <ShakingHands
+                    :height="43"
+                    :width="43"
+                ></ShakingHands>
+            </div>
+            <h1 class="fw-bold title" v-bind:class="[dark_mode ? 'text-white':'primary-font']" >Francess <span class="secondary-font fw-bold">Cillo</span></h1>
+            <div class="changing-roles d-flex m-auto">
+                <h5 class="fw-semibold intro-title m-0 me-3" v-bind:class="[dark_mode ? 'text-white':'primary-font']">Im A</h5>
+                <h5 class="fw-semibold intro-title m-0" v-bind:class="[dark_mode ? 'text-white':'primary-font']">Front End Developer</h5>
+            </div>
+        </div>
+        <div class="chatbox h-100">
+
+        </div>
     </div>
-    
 </template>
 
 <script>
+    import ShakingHands from '../components/icons/ShakingHands.vue'
     export default {
+        components:{
+            ShakingHands
+        },
         props: {
             dark_mode: Boolean
         }    
@@ -16,5 +38,11 @@
 <style>
     .hero{
         min-height: 100vh
+    }
+    .intro-title{
+        font-size: 2rem;
+    }
+    .title{
+        font-size: 6rem;
     }
 </style>
